7427ed59f539ba473ffb6c535e28890083ae1ffa
ClareMcDonald <clare.s.mcdonald@gmail.com>
2021-11-30 10:10:55 -0800

    test working

--- test/functions.test.js
+++ test/functions.test.js
@@ -1,16 +1,22 @@
 // IMPORT MODULES under test here:
 import { 
-    myFunction,
+    addExclamationPoints,
 } from '../functions.js';
 
 const { test, skip } = QUnit;
 
-test('this test should pass', (expect) => {
-    const expected = true;
+test('addExclaimationPoints should return a string with 3 exclaimation points added to the end', (expect) => {
+    const expected1 = 'puppy!!!';
 
-    const actual = myFunction();
+    const actual1 = addExclamationPoints('puppy');
 
-    expect.equal(actual, expected);
+    expect.equal(actual1, expected1);
+
+    const expected2 = 'guppy!!!';
+
+    const actual2 = addExclamationPoints('guppy');
+
+    expect.equal(actual2, expected2);
 });
 
 
